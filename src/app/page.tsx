16d44b3359b7355f97dcbe1bbdf034ef5d3f91ec
Vo Tran Phi <songokupkj@gmail.com>
2024-10-27 15:36:29 +0700

    fix: window is not defined due to SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import Image from "next/image";
+import dynamic from "next/dynamic";
+
+const DeploymentModelWithNoSSR = dynamic(
+  () => import('../components/DeploymentModel'),
+  { ssr: false }
+)
 
 export default function Home() {
   return (
-    <div style={{width: `${window.innerWidth}px`, height: `${window.innerHeight}px`}}>
-      <Image alt="Deployment Model" width={1438} height={542} src={"/deployment_model.svg"} style={{position: "relative", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} />
-    </div>
+    <DeploymentModelWithNoSSR />
   );
 }
