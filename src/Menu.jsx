c1df2bf7b7d1468ee4b25f3a56a232376d3ff50c
minhphuc2544 <phucle190904@gmail.com>
2024-12-08 20:56:53 +0700

    refactor: delete unused css code of Menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,7 +1,7 @@
 import "./styles/Menu.css"
 export default function Menu() {
     return (
-        <div>
+        <div className="body">
             <nav class="sidebar">
                 
                 <div class="sidebar-links">
