5e184ae9473bb916997b8ed3751e5400293b6dc3
minhphuc2544 <phucle190904@gmail.com>
2024-12-09 21:20:00 +0700

    feat: home UI layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
-export default function Home(){
-    return(
-        <div></div>
+import "../styles/Home.css"
+
+
+export default function Home() {
+    return (
+        <div className="home">
+            <div className="upper">
+                <div className="upper wordSets">
+
+                </div>
+                <div className="userInfo">
+
+                </div>
+            </div>
+
+            <div className="statistic">
+
+            </div>
+        </div>
     );
 }
