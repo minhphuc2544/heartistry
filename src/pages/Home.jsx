c1ed7e800d7d3ae141bda1340a9e0a0556be1b3f
minhphuc2544 <phucle190904@gmail.com>
2024-12-09 23:11:17 +0700

    feat: dashboard word sets area UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,41 @@
 import "../styles/Home.css"
 
-
 export default function Home() {
     return (
         <div className="home">
             <div className="upper">
-                <div className="upper wordSets">
 
+                <div className="wordSets">
+                    <div style={{ display: "flex"}}>
+                        <h1 className="title">Word Sets</h1> 
+                        <div className="moveList"> {/*add type for button: move list of wordsets if there are more wordsets than the numbers of wordsets tha the area can show (currently: 3) */}
+                            <input type="image" src="../focused_leftArrow.svg"></input>
+                            <input type="image" src="../focused_rightArrow.svg"></input>
+                        </div>
+                    </div>
+
+                    <div style={{ display: "flex" }}>
+                        <div className="set">
+                            <p className="topic">Education</p>
+                            <p className="wordNumbers">Number of words: $(NoW)</p>  {/*show number of words in this wordset*/}
+                            <button type="" id="learn">Learn</button>  {/*add type for button: begin to learn words in wordset*/}
+                        </div>
+
+
+                        <div className="set">
+
+                        </div>
+
+                        <div className="set">
+
+                        </div>
+
+
+                    </div>
                 </div>
+
+
+
                 <div className="userInfo">
 
                 </div>
