3d2795be04c1970231fe5aa3cd37edd996cea238
Vo Tran Phi <songokupkj@gmail.com>
2024-12-11 11:40:46 +0700

    feat(h-11): apply api for pulling wordset

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import "../styles/Home.css"
 import Cookies from "js-cookie";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
     const navigate = useNavigate();
+    const PAGE_SIZE = 4;
+    const [page, setPage] = useState(0);
+    const [wordSets, setWordSets] = useState([]);
     
     // check if the access token is expired, if so, force the user to login again
     useEffect(() => {
@@ -14,6 +17,28 @@ export default function Home() {
         }
     }, []);
 
+    // useEffect uses to get a page of wordset
+    useEffect(() => {
+        async function getWordSetPage() {
+
+            // call api
+            const response = await fetch(`${import.meta.env.VITE_TASK_API_BASE_URL}/wordsets/me?page=${page}&pageSize=${PAGE_SIZE}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${Cookies.get('access_token')}`
+                    }
+                }
+            );
+
+            const responseJson = await response.json();
+            
+            setWordSets(responseJson);
+        }
+
+        getWordSetPage()
+    }, [page])
+
     return (
         <div className="home">
             <div className="upper">
@@ -22,32 +47,14 @@ export default function Home() {
                     <div style={{ display: "flex" }}>
                         <h1 className="title">Word Sets</h1>
                         <div className="moveList"> {/*add type for button: move list of wordsets if there are more wordsets than the numbers of wordsets tha the area can show (currently: 4) */}
-                            <input type="image" src="../disabled_leftArrow.svg"></input>
-                            <input type="image" src="../enabled_rightArrow.svg"></input>
+                            <input type="image" src="../disabled_leftArrow.svg" onClick={ () => page > 0 && setPage(page - 1) }></input>
+                            <p style={{ display: "inline" }}>{ page + 1 }</p>
+                            <input type="image" src="../enabled_rightArrow.svg" onClick={ () => wordSets.length && setPage(page + 1) }></input>
                         </div>
                     </div>
 
                     <div style={{ display: "flex" }}>
-                        <div className="set">
-                            <p className="topic">Education</p>
-                            <p className="wordNumbers">Number of words: $(NoW)</p>  {/*show number of words in this wordset*/}
-                            <button type="" id="learn">Learn</button>  {/*add type for button: begin to learn words in wordset*/}
-                        </div>
-                        <div className="set">
-                            <p className="topic">Education</p>
-                            <p className="wordNumbers">Number of words: $(NoW)</p>  {/*show number of words in this wordset*/}
-                            <button type="" id="learn">Learn</button>  {/*add type for button: begin to learn words in wordset*/}
-                        </div>
-                        <div className="set">
-                            <p className="topic">Education</p>
-                            <p className="wordNumbers">Number of words: $(NoW)</p>  {/*show number of words in this wordset*/}
-                            <button type="" id="learn">Learn</button>  {/*add type for button: begin to learn words in wordset*/}
-                        </div>
-                        <div className="set">
-                            <p className="topic">Education</p>
-                            <p className="wordNumbers">Number of words: $(NoW)</p>  {/*show number of words in this wordset*/}
-                            <button type="" id="learn">Learn</button>  {/*add type for button: begin to learn words in wordset*/}
-                        </div>
+                        { wordSets.length !== 0 ? wordSets.map((v, i) => <WordSetCard key={i} wordSetInfo={v} />) : <p>There's no more wordsets</p> }
                     </div>
                 </div>
 
@@ -73,6 +80,16 @@ export default function Home() {
             <div className="statistic">
 
             </div>
+        </div>
+    );
+}
+
+function WordSetCard({ wordSetInfo }) {
+    return (
+        <div className="set">
+            <p className="topic">{ wordSetInfo.topic }</p>
+            <p className="wordNumbers">Number of words: { wordSetInfo.noWords }</p>  {/*show number of words in this wordset*/}
+            <button type="" id="learn">Learn</button>  {/*add type for button: begin to learn words in wordset*/}
         </div>
     );
 }
