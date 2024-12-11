7e26de8051739684afb6868255e80bf287dac11e
Vo Tran Phi <songokupkj@gmail.com>
2024-12-12 06:01:39 +0700

    feat(h-12): change existing words

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import "../styles/Home.css"
 import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+    // for API's purpose
     const navigate = useNavigate();
-    const PAGE_SIZE = 4;
-    const [page, setPage] = useState(0);
+    const WORDSET_PAGE_SIZE = 4;
+    const [wordSetPage, setWordSetPage] = useState(0);
     const [wordSets, setWordSets] = useState([]);
-    const [lastPage, setLastPage] = useState(0);
-    const [isVisible, setVisible] = useState(true); //show info and button before learn words in word set
-    const [isTurn, setTurn] = useState(false); //to change the info on the card when user click (flip the card)
+    const [wsLastPage, setWsLastPage] = useState(0);
+    const [learningWordSet, setLearningWordSet] = useState({}); 
+    // for UI's purpose
     const [isWordSetOpen, setWordSetOpen] = useState(false); //check if word set is opened
-    const [isEditWordSet, setWordSetEdit] = useState(false); // check if user is editing word set
     const [isAddNewWord, setAddNewWord] = useState(false); //check if user is adding new word to word set
   
     // check if the access token is expired, if so, force the user to login again
     useEffect(() => {
         const access_token = Cookies.get('access_token');
         if (!access_token) {
-            // navigate('/login');
+            navigate('/login');
         }
     }, []);
 
@@ -28,23 +28,22 @@ export default function Home() {
         async function getWordSetPage() {
 
             // call api
-            const response = await fetch(`${import.meta.env.VITE_TASK_API_BASE_URL}/wordsets/me?page=${page}&pageSize=${PAGE_SIZE}`, {
+            const response = await fetch(`${import.meta.env.VITE_TASK_API_BASE_URL}/wordsets/me?page=${wordSetPage}&pageSize=${WORDSET_PAGE_SIZE}`, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
                     "Authorization": `Bearer ${Cookies.get('access_token')}`
                 }
-            }
-            );
+            });
 
             const responseJson = await response.json();
 
             setWordSets(responseJson.response);
-            setLastPage(Math.floor(responseJson.pagination.total / PAGE_SIZE));
+            setWsLastPage(Math.floor(responseJson.pagination.total / WORDSET_PAGE_SIZE));
         }
 
         getWordSetPage()
-    }, [page])
+    }, [wordSetPage])
 
     return (
         <div className="home">
@@ -54,14 +53,14 @@ export default function Home() {
                     <div style={{ display: "flex" }}>
                         <h1 className="title">Word Sets</h1>
                         <div className="moveList"> {/*add type for button: move list of wordsets if there are more wordsets than the numbers of wordsets tha the area can show (currently: 4) */}
-                            <input type="image" src="../disabled_leftArrow.svg" onClick={ () => page > 0 && setPage(page - 1) }></input>
-                            <p style={{ display: "inline" }}>{ page + 1 }</p>
-                            <input type="image" src="../enabled_rightArrow.svg" onClick={ () => page < lastPage && setPage(page + 1) }></input>
+                            <input type="image" src="../disabled_leftArrow.svg" onClick={ () => wordSetPage > 0 && setWordSetPage(wordSetPage - 1) }></input>
+                            <p style={{ display: "inline" }}>{ wordSetPage + 1 }</p>
+                            <input type="image" src="../enabled_rightArrow.svg" onClick={ () => wordSetPage < wsLastPage && setWordSetPage(wordSetPage + 1) }></input>
                         </div>
                     </div>
 
                     <div style={{ display: "flex" }}>
-                        { wordSets.length !== 0 ? wordSets.map((v, i) => <WordSetCard key={i} wordSetInfo={v} setWordSetOpen={setWordSetOpen} />) : <p>There's no wordsets</p> }
+                        { wordSets.length ? wordSets.map((v, i) => <WordSetCard key={i} wordSetInfo={v} setWordSetOpen={setWordSetOpen} setLearningWordSet={setLearningWordSet} />) : <p>There's no wordsets</p> }
                     </div>
                 </div>
 
@@ -87,35 +86,94 @@ export default function Home() {
             <div className="statistic">
 
             </div>
-            <WordSetPopUp isWordSetOpen={isWordSetOpen} isTurn={isTurn} isVisible={isVisible} setVisible={setVisible} setTurn={setTurn} setWordSetOpen={setWordSetOpen} isEditWordSet={isEditWordSet} setWordSetEdit={setWordSetEdit} isAddNewWord={isAddNewWord} setAddNewWord={setAddNewWord} />
+            <WordSetPopUp learningWordSet={learningWordSet} isWordSetOpen={isWordSetOpen} setWordSetOpen={setWordSetOpen} setAddNewWord={setAddNewWord} />
             <AddNewWord isAddNewWord={isAddNewWord} setAddNewWord={setAddNewWord} />
-
         </div>
 
     );
 }
 
-function WordSetCard({ wordSetInfo, setWordSetOpen }) {
-    return (
-        <div className="set">
-            <p className="topic">{wordSetInfo.topic}</p>
-            <p className="wordNumbers">Number of words: {wordSetInfo.noWords}</p>  {/*show number of words in this wordset*/}
-            <button type="" id="learn" onClick={() => setWordSetOpen(true)}>Learn</button>  {/*add type for button: begin to learn words in wordset*/}
-        </div>
-    );
-}
+function WordSetPopUp({ learningWordSet, isWordSetOpen, setWordSetOpen, setAddNewWord}) {
+    // for API's purpose
+    const WORD_PAGE_SIZE = 10;
+    const [wordPage, setWordPage] = useState(0);
+    const [words, setWords] = useState([]);
+    const [wLastPage, setWLastPage] = useState(0);
+    const [needUpdate, setNeedUpdate] = useState(false);
+    const [changedWords, setChangedWords] = useState([]);
+    // for UI's purpose
+    const [isEditWordSet, setWordSetEdit] = useState(false); // check if user is editing word set
+    const [isVisible, setVisible] = useState(true); //show info and button before learn words in word set
+    const [isTurn, setTurn] = useState(false); //to change the info on the card when user click (flip the card)
 
-function WordSetPopUp({ isWordSetOpen, isTurn, isVisible, setVisible, setTurn, setWordSetOpen, isEditWordSet, setWordSetEdit, isAddNewWord, setAddNewWord})
-{
+    // useEffect uses to get word page
+    useEffect(() => {
+        async function getWordPage() {
+            // reset the words list for not caching
+            setWords([]);
+
+            // call api
+            const response = await fetch(`${import.meta.env.VITE_TASK_API_BASE_URL}/words/${learningWordSet.id}?page=${wordPage}&pageSize=${WORD_PAGE_SIZE}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${Cookies.get('access_token')}`
+                }
+            });
+
+            const responseJson = await response.json();
+
+            setWords(responseJson.response);
+            setWLastPage(Math.floor(responseJson.pagination.total / WORD_PAGE_SIZE));
+        }
+
+        // set words if the edit page is opened
+        if (isEditWordSet) {
+            getWordPage();
+            return;
+        }
+
+        // set words to empty if the edit page is closed
+        setWords([]);
+    }, [isEditWordSet, wordPage])
+
+    // useEffect uses to update changed words
+    useEffect(() => {
+        async function updateWords(id, word, note) {
+            const requestBody = {
+                "word": word,
+                "note": note,
+            }
+
+            // call api
+            const response = await fetch(`${import.meta.env.VITE_TASK_API_BASE_URL}/words/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${Cookies.get('access_token')}`
+                },
+                body: JSON.stringify(requestBody),
+            });
+        }
+
+        if (needUpdate) {
+            for (let word of changedWords) {
+                updateWords(word.id, word.word, word.note);
+            }
+        }
+        
+        setNeedUpdate(false);
+    }, [needUpdate])
+
     return (<>
         {
             isWordSetOpen &&
             <div className="pop_up">
-                <input type="image" className="unfocused_cancel" src="./unfocused_cancel.svg" onClick={() => { setWordSetOpen(false); setVisible(true); setTurn(false) }}></input>
+                <input type="image" className="unfocused_cancel" src="./unfocused_cancel.svg" onClick={() => { setWordSetOpen(false); setVisible(true); setTurn(false); setAddNewWord(false); setWordSetEdit(false); setWordPage(0) }}></input>
                 {isVisible ? <>
-                    <h1 className="wordSet_topic">Education</h1>
-                    <p className="vcb_count">Vocabulary count: 234 words</p>
-                    <button className="start" onClick={() => setVisible(!isVisible)}>Start</button>
+                    <h1 className="wordSet_topic">{ learningWordSet.topic }</h1>
+                    <p className="vcb_count">Vocabulary count: { learningWordSet.noWords }</p>
+                    <button className="start" onClick={() => setVisible(false)}>Start</button>
                     <button className="editWordSet" onClick={() => setWordSetEdit(true)}>Edit word set</button>
                 </> : <>
                     <div className="card" onClick={() => setTurn(!isTurn)}>
@@ -123,7 +181,7 @@ function WordSetPopUp({ isWordSetOpen, isTurn, isVisible, setVisible, setTurn, s
                             isTurn ? <>
                                 <div className="back">
                                     <div style={{ display: "flex", justifyContent: "center", fontSize: 40, marginBottom: 20 }}>
-                                        <p className="word">Flash card</p>
+                                        <p className="word">{  }</p>
                                         <p className="wordType">(n)</p>
                                     </div>
                                     <div style={{ display: "flex" }}>
@@ -147,25 +205,21 @@ function WordSetPopUp({ isWordSetOpen, isTurn, isVisible, setVisible, setTurn, s
                     isEditWordSet &&
                     <>
                         <div className="editWS">
-                            <input type="text" className="editTopic" defaultValue={"Education"}></input>
+                            <input type="text" className="editTopic" defaultValue={learningWordSet.topic}></input>
                             <div style={{ display: "flex", justifyContent: "center", margin: 20 }}>
-                                <input type="image" src="./disabled_leftArrow.svg"></input>
+                                <input type="image" src="./disabled_leftArrow.svg"  onClick={ () => wordPage > 0 && setWordPage(wordPage - 1) }></input>
                                 {/* <p style={{ display: "inline" }}>{ page + 1 }</p> */}
-                                <input type="image" src="./enabled_rightArrow.svg"></input>
-                                <input type="image" className="unfocused_cancel" src="./unfocused_cancel.svg" onClick={() => { setWordSetEdit(false) }}></input>
+                                <input type="image" src="./enabled_rightArrow.svg"  onClick={ () => wordPage < wLastPage && setWordPage(wordPage + 1) }></input>
+                                <p style={{ display: "inline" }}>{ wordPage + 1 }</p>
+                                <input type="image" className="unfocused_cancel" src="./unfocused_cancel.svg" onClick={() => { setWordSetEdit(false); setAddNewWord(false) }}></input>
                             </div>
 
                             <div className="wordList">
-                                <div style={{ display: "flex" }}> {/*add this div to add word in this list */}
-                                    <input type="text" className="editInfo" defaultValue={"word1"}></input>
-                                    <input type="text" className="editInfo" defaultValue={"note1"}></input>
-                                    <input type="image" className="deleteWord" src="./unfocused_cancel.svg" style={{ padding: "1px" }}></input>
-                                </div>
-
+                                { words.length ? words.map((v, i) => <WordRow key={i} wordInfo={v} setChangedWords={setChangedWords} />) : <p>There's no word</p> }
                             </div>
                             <div style={{ display: "flex" }}>
                                 <button className="editBtn" style={{ backgroundColor: "#81C784" }} onClick={() => { setAddNewWord(true) }}>Add new word</button>
-                                <button className="editBtn " style={{ backgroundColor: "#FFEB3B" }}>Apply change</button>
+                                <button className="editBtn" style={{ backgroundColor: "#FFEB3B" }} onClick={ () => setNeedUpdate(true) }>Apply change</button>
                             </div>
                         </div>
                     </>
@@ -204,5 +258,76 @@ function AddNewWord({ isAddNewWord, setAddNewWord }) {
                 </div>
             }
         </>
+    )
+}
+
+function WordSetCard({ wordSetInfo, setWordSetOpen, setLearningWordSet }) {
+    return (
+        <div className="set">
+            <p className="topic">{wordSetInfo.topic}</p>
+            <p className="wordNumbers">Number of words: {wordSetInfo.noWords}</p>  {/*show number of words in this wordset*/}
+            <button type="" id="learn" onClick={() => { setWordSetOpen(true); setLearningWordSet(wordSetInfo) }}>Learn</button>  {/*add type for button: begin to learn words in wordset*/}
+        </div>
+    );
+}
+
+function WordRow({ wordInfo, setChangedWords }) {
+    const newWord = useRef(wordInfo);
+    const isChanged = useRef(false);
+
+    return (
+        <div style={{ display: "flex" }}> {/*add this div to add word in this list */}
+            <input
+                type="text"
+                className="editInfo"
+                defaultValue={wordInfo.word}
+                required
+                onBlur={ (e) => setChangedWords(
+                    (old) => {
+                        if (isChanged.current) {
+                            const exists = old.some(item => item.id === wordInfo.id);
+                            newWord.current = {
+                                ...newWord.current,
+                                word: e.target.value,
+                            }
+                            isChanged.current = false;
+                            if (exists) {
+                                return old.map(item => item.id === newWord.current.id ? newWord.current : item);
+                            } else {
+                                return [...old, newWord.current];
+                            }
+                        }
+                        return old;
+                    } 
+                )}
+                onChange={ () => isChanged.current = true }
+            ></input>
+            <input
+                type="text"
+                className="editInfo"
+                defaultValue={wordInfo.note}
+                required
+                onBlur={ (e) => setChangedWords(
+                    (old) => {
+                        if (isChanged.current) {
+                            const exists = old.some(item => item.id === wordInfo.id);
+                            newWord.current = {
+                                ...newWord.current,
+                                note: e.target.value,
+                            }
+                            isChanged.current = false;
+                            if (exists) {
+                                return old.map(item => item.id === newWord.current.id ? newWord.current : item);
+                            } else {
+                                return [...old, newWord.current];
+                            }
+                        }
+                        return old;
+                    } 
+                )}
+                onChange={ () => isChanged.current = true }
+            ></input>
+            <input type="image" className="deleteWord" src="./unfocused_cancel.svg" style={{ padding: "1px" }}></input>
+        </div>
     )
 }
