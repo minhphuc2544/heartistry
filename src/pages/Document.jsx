2b0cc41dd9a42eb80e6d18feeb5a3ee0e5dbcea8
minhphuc2544 <phucle190904@gmail.com>
2024-12-19 22:36:29 +0700

    feat: document draft UI and adjust dictionary margin UI

--- src/pages/Document.jsx
+++ src/pages/Document.jsx
@@ -1,5 +1,28 @@
-export default function Document(){
-    return(
-        <div></div>
+import "../styles/Document.css";
+export default function Document() {
+    return (
+        <div className="document">
+            <div style={{ display: "flex" }}>
+
+                <div className="documentArea">
+                        
+                </div>
+                <div className="userInfo">
+                    <img className="userPicture" src="./logo.svg"></img> {/*user avatar*/}
+                    <p className="username">Heartistry</p> {/*username*/}
+                    <div className="duration">
+                        <div style={{ display: "block", margin: 20 }}>
+                            <p>Words</p>
+                            <p>100</p>
+                        </div>
+                        <div className="dashboard_separator"></div>
+                        <div style={{ display: "block", margin: 20 }}>
+                            <p>Days</p>
+                            <p>90</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
