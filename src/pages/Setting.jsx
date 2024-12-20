6aca778d268c16da940d5a84c383cf025f616d3d
Vo Tran Phi <songokupkj@gmail.com>
2024-12-21 03:12:38 +0700

    reformat: split components

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -2,7 +2,6 @@ import "../styles/Setting.css";
 import { useEffect, useState } from "react";
 
 export default function Setting() {
-    const [isChangePassword, setChangePassword] = useState(false);
     return (
         <div className="setting">
             <div style={{ display: "flex", backgroundColor: "#21B6A8", marginTop: "20px", borderRadius: "22px" }}>
@@ -28,40 +27,71 @@ export default function Setting() {
                 </div>
             </div>
 
-            <div className="userUpdateInfo">
-                <h1 style={{ fontSize: "45px", fontFamily: "sans-serif", margin: "20px" }}>Profile</h1>
-                <div style={{ display: "flex", height: "400px", justifyContent: "center" }}>
-                    <div className="updateInfo">
+            <UpdateInfoZone />
+        </div>
+    );
+}
+
+function UpdateInfoZone({  }) {
+    const [isChangePassword, setChangePassword] = useState(false);
+    return (
+        <div className="userUpdateInfo">
+            <h1 style={{ fontSize: "45px", fontFamily: "sans-serif", margin: "20px" }}>Profile</h1>
+            <div style={{ display: "flex", height: "400px", justifyContent: "center" }}>
+                <div className="updateInfo">
+                    <div>
                         <label className="label">Username</label><br></br>
                         <input type="text" className="input" id="username" defaultValue={"username"} required style={{ marginBottom: 10, pointerEvents: "none", backgroundColor: "lightgrey" }} /><br></br>
+                    </div>
+                    <div>
                         <label className="label">Email</label><br></br>
                         <input type="text" className="input" id="username" required style={{ marginBottom: 10 }} /><br></br>
+                    </div>
+                    <div>
                         <label className="label">Phone number</label><br></br>
                         <input type="text" className="input" id="username" required style={{ marginBottom: 10 }} /><br></br>
-                        <button className="updateBtn">Update</button>
                     </div>
-                    <div className="changePassword">
-                        <>
-                            {
-                                isChangePassword ?
-                                <div>
-                                    <label className="label">Current password</label><br></br>
-                                    <input type="text" className="input" id="username" required style={{ marginBottom: 10 }} /><br></br>
-                                    <label className="label">New password</label><br></br>
-                                    <input type="text" className="input" id="username" required style={{ marginBottom: 10 }} /><br></br>
-                                    <label className="label">Confirm new password</label><br></br>
-                                    <input type="text" className="input" id="username" required style={{ marginBottom: 10 }} /><br></br>
-                                    <button className="changepwdBtn">Change</button>
-                                </div>
-                                :
-                                
-                                                                <button className="changepwdBtn" onClick={() => setChangePassword(!isChangePassword)}>Change password</button>
+                    <div>
+                        <label className="label">Full name</label><br></br>
+                        <input className="input" type="text" required></input>
+                    </div>
+                    <div>
+                        <label className="label">Gender</label><br></br>
+                        <select className="optionInput">
+                            <option value={"male"}>Male</option>
+                            <option value={"female"}>Female</option>
+                            <option value={"unspecified"}>I prefer not to say</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label className="label">Date of Birth</label><br></br>
+                        <input className="DoBInput" type="date" required></input>
+                    </div>
 
-                            }
-                        </>
-                    </div>
+                    <button className="updateBtn">Update</button>
+                </div>
+                <div className="changePassword">
+                    {
+                        isChangePassword ?
+                        <ChangePassZone /> :
+                        <button className="changepwdBtn" onClick={() => setChangePassword(!isChangePassword)}>Change password</button>
+                    }
                 </div>
             </div>
+        </div>
+    )
+}
+
+function ChangePassZone({  }) {
+    return (
+        <div>
+            <label className="label">Current password</label><br></br>
+            <input type="text" className="input" id="username" required style={{ marginBottom: 10 }} /><br></br>
+            <label className="label">New password</label><br></br>
+            <input type="text" className="input" id="username" required style={{ marginBottom: 10 }} /><br></br>
+            <label className="label">Confirm new password</label><br></br>
+            <input type="text" className="input" id="username" required style={{ marginBottom: 10 }} /><br></br>
+            <button className="changepwdBtn">Change</button>
         </div>
     );
 }
