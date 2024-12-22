18aa832415d7bd678f6f615948eb5c0a8f6f7b46
minhphuc2544 <phucle190904@gmail.com>
2024-12-23 00:34:40 +0700

    fix: adjust signup UI

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -63,42 +63,42 @@ export default function SignUp() {
         <div className="signup">
             <div className="rightPart">
                 <div className="signUpArea">
-                    <p style={{ fontSize: 50, textAlign: "center", fontFamily: "'Segoe UI'", margin: "auto 0" }}>Create new account</p>
+                    <p style={{ fontSize: 50, textAlign: "center", fontFamily: "'Segoe UI'", position: "fixed", bottom: "770px" }}>Create new account</p>
                     <form className="signUpForm">
                         <div>
-                            <label className="label">Username</label><br></br>
-                            <input className="input" type="text" required onChange={ (e) => setUsername(e.target.value) }></input>
+                            <label className="signup_label">Username</label><br></br>
+                            <input className="signup_input" type="text" required onChange={ (e) => setUsername(e.target.value) }></input>
                         </div>
                         <div>
-                            <label className="label">Password</label><br></br>
-                            <input className="input" type="password" required onChange={ (e) => setPassword(e.target.value) }></input>
+                            <label className="signup_label">Password</label><br></br>
+                            <input className="signup_input" type="password" required onChange={ (e) => setPassword(e.target.value) }></input>
                         </div>
                         <div>
-                            <label className="label">Full name</label><br></br>
-                            <input className="input" type="text" required onChange={ (e) => setFullname(e.target.value) }></input>
+                            <label className="signup_label">Full name</label><br></br>
+                            <input className="signup_input" type="text" required onChange={ (e) => setFullname(e.target.value) }></input>
                         </div>
                         <div>
-                            <label className="label">Gender</label><br></br>
-                            <select className="optionInput" onChange={ (e) => setGender(e.target.value) }>
+                            <label className="signup_label">Gender</label><br></br>
+                            <select className="signup_optionInput" onChange={ (e) => setGender(e.target.value) }>
                                 <option value={"male"}>Male</option>
                                 <option value={"female"}>Female</option>
                                 <option value={"unspecified"}>I prefer not to say</option>
                             </select>
                         </div>
                         <div>
-                            <label className="label">Date of Birth</label><br></br>
-                            <input className="DoBInput" type="date" required onChange={ (e) => setDob(e.target.value) }></input>
+                            <label className="signup_label">Date of Birth</label><br></br>
+                            <input className="signup_DoBInput" type="date" required onChange={ (e) => setDob(e.target.value) }></input>
                         </div>
                         <div>
-                            <label className="label">Email</label><br></br>
-                            <input className="input" type="email" required onChange={ (e) => setEmail(e.target.value) }></input>
+                            <label className="signup_label">Email</label><br></br>
+                            <input className="signup_input" type="email" required onChange={ (e) => setEmail(e.target.value) }></input>
                         </div>
                         <div>
-                            <label className="label">Phone number</label><br></br>
-                            <input className="input" type="tel" pattern="0[0-9]{9}" required onChange={ (e) => setPhoneNumber(e.target.value) }></input>
+                            <label className="signup_label">Phone number</label><br></br>
+                            <input className="signup_input" type="tel" pattern="0[0-9]{9}" required onChange={ (e) => setPhoneNumber(e.target.value) }></input>
                         </div>
                         <div className="action">
-                            <input type="button" id="submit" onClick={() => setSubmitSignal(!submitSignal)} value={"SIGN UP"}></input><br></br>
+                            <input type="button" id="signup_submit" onClick={() => setSubmitSignal(!submitSignal)} value={"SIGN UP"}></input><br></br>
                         </div>
                     </form>
                 </div>
