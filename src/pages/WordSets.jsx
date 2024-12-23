369e24f6c3062eade8181661dbbb8320ea14f1e0
minhphuc2544 <phucle190904@gmail.com>
2024-12-24 04:19:01 +0700

    feat: prepare word sets'ui for admin function

--- src/pages/WordSets.jsx
+++ src/pages/WordSets.jsx
@@ -12,6 +12,7 @@ export default function WordSets() {
                                 <th>Number of words</th>
                                 <th>Is recommended</th>
                                 <th>Add to recommend</th>
+                                <th>Delete from recommend</th>
                             </tr>
                         </thead>
                         <tbody className="wordsets_table-content">
@@ -19,16 +20,22 @@ export default function WordSets() {
                                 { ID: "1", idUser: "1", topic: "Education", noWords: "120", isRecommended: true },
                                 { ID: "2", idUser: "2", topic: "Environment", noWords: "50", isRecommended: false },
                             ].map((item, index) => (
-                                <tr key={index}>
+                                <tr contentEditable='true' key={index}>
                                     <td>{item.ID}</td>
                                     <td>{item.idUser}</td>
                                     <td>{item.topic}</td>
                                     <td>{item.noWords}</td>
                                     <td>{item.isRecommended ? "true" : "false"}</td>
-                                    <td>
+                                    <td width={200} align="center">
                                         {
                                             !item.isRecommended &&
                                             <input type="image" src="../approved.svg" style={{ backgroundColor: "#34B233", borderRadius: "50%", width: "30px", height: "30px", marginRight: "10px" }}></input>
+                                        }
+                                    </td>
+                                    <td  width={250} align="center"> 
+                                        {
+                                            item.isRecommended &&
+                                            <input type="image" src="../disapproved.svg" style={{ backgroundColor: "red", borderRadius: "50%", width: "30px", height: "30px", marginRight: "10px" }}></input>
                                         }
                                     </td>
                                 </tr>
@@ -36,6 +43,7 @@ export default function WordSets() {
                         </tbody>
                     </table>
                 </div>
+                <input type="image" src="../add_wordset.svg" style={{ backgroundColor: "#34B233", borderRadius: "50%", width: "60px", height: "60px", marginRight: "10px", position: "fixed", top: "850px", left: "1854px" }}></input>
             </div>
     )
 }
