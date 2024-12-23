980de351ad4ba45716eeeed1ded6b8686efddc51
minhphuc2544 <phucle190904@gmail.com>
2024-12-24 04:11:29 +0700

    feat: prepare user'ui for admin function

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -17,72 +17,15 @@ export default function User() {
                             <th>Role</th>
                             <th>Created Day</th>
                             <th>Last online day</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody className="user_table-content">
                         {[
                             { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "2", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "3", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "4", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "5", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "6", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
-                            { ID: "1", fullName: "Lê Dương Minh Phúc", username: "minhphuc2544", email: "phucle190904@gmail.com", phoneNumber: "0339161863", dateOfBirth: "09/19/2004", gender: "male", role: "admin", createdDay: "09/19/2021", lastOnlineDay: "09/19/2021" },
                         ].map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.ID}</td>
+                            <tr contentEditable='true' key={index}>
+                                <td contentEditable='true' >{item.ID}</td>
                                 <td>{item.fullName}</td>
                                 <td>{item.username}</td>
                                 <td>{item.email}</td>
@@ -92,11 +35,15 @@ export default function User() {
                                 <td>{item.role}</td>
                                 <td>{item.createdDay}</td>
                                 <td>{item.lastOnlineDay}</td>
+                                <td style={{ display: "flex", justifyContent: "center" }}>
+                                    <input type="image" src="../disapproved.svg" style={{ backgroundColor: "red", borderRadius: "50%", width: "30px", height: "30px", marginRight: "10px" }}></input>                                
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
+            <input type="image" src="../add_wordset.svg" style={{ backgroundColor: "#34B233", borderRadius: "50%", width: "60px", height: "60px", marginRight: "10px", position: "fixed", top: "850px", left: "1854px" }}></input>
         </div>
     );
 }
