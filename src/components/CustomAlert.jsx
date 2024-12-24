fcfb3b419dd190fab68075c38bce1a56f80b93fc
minhphuc2544 <phucle190904@gmail.com>
2024-12-24 15:00:35 +0700

    feat: custom confirm alert

--- src/components/CustomAlert.jsx
+++ src/components/CustomAlert.jsx
@@ -1,12 +1,12 @@
 import "../styles/CustomAlert.css";
-export default function CustomAlert({ message }) {
+export default function CustomAlert({ message, okHandler }) {
   return (
         <div className="alert">
             <div className="msg">
                 {message}
             </div>
             <div>
-                <button className="alert_button">OK</button>
+                <button className="alert_button" onClick={okHandler}>OK</button>
             </div>
         </div>
   );
