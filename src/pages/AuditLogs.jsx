9db3e66ca9980f630d60189f37891aefb54fe724
minhphuc2544 <phucle190904@gmail.com>
2024-12-25 04:13:56 +0700

    feat: get all audit logs for admin

--- src/pages/AuditLogs.jsx
+++ src/pages/AuditLogs.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import "../styles/AuditLogs.css";
 
 export default function AuditLogs() {
     const navigate = useNavigate();
-
+    const [auditLogs, setAuditLogs] = useState([]);
     // check if the token is existed and user has 'user' role
     useEffect(() => {
         const access_token = Cookies.get('access_token');
@@ -19,42 +20,72 @@ export default function AuditLogs() {
         }
     }, []);
 
+    useEffect(() => {
+        async function fetchAuditLogs() {
+            const userResponseJson = await fetch(`${import.meta.env.VITE_USER_API_BASE_URL}/audit-logs/all`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${Cookies.get('access_token')}`
+                }
+            });
+
+            const taskResponseJson = await fetch(`${import.meta.env.VITE_TASK_API_BASE_URL}/audit-logs/all`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${Cookies.get('access_token')}`
+                }
+            });
+
+            if (userResponseJson.ok && taskResponseJson.ok) {
+                const data1 = await userResponseJson.json();
+                const data2 = await taskResponseJson.json();
+                setAuditLogs([...data1, ...data2]);
+            }
+        }
+        fetchAuditLogs();
+    }, []);
+
+
     return (
         <div className="auditlogs_table-container">
-                <div className="auditlogs_table-body">
-                    <table>
-                        <thead className="auditlogs_table-header">
-                            <tr>
-                                <th>ID</th>
-                                <th>Action</th>
-                                <th>Entity</th>
-                                <th>Entity ID</th>
-                                <th>User ID</th>
-                                <th>Username</th>
-                                <th>Role</th>
-                                <th>Details</th>
-                                <th>Timestamp</th>
-                            </tr>
-                        </thead>
-                        <tbody className="auditlogs_table-content">
-                            {[
-                                { ID: "1", action: "CREATE", Entity: "Wordset", entityId: "1", userId: "1", username: "minhphuc2544", role: "admin", details: "Created wordset", timestamp: "2021-09-01 12:00:00" },
-                            ].map((item, index) => (
-                                <tr key={index}>
-                                    <td>{item.ID}</td>
-                                    <td>{item.action}</td>
-                                    <td>{item.Entity}</td>
-                                    <td>{item.entityId}</td>
-                                    <td>{item.userId}</td>
-                                    <td>{item.username}</td>
-                                    <td>{item.role}</td>
-                                    <td>{item.details}</td>
-                                    <td>{item.timestamp}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+            <div className="auditlogs_table-body">
+                <table>
+                    <thead className="auditlogs_table-header">
+                        <tr>
+                            <th>ID</th>
+                            <th>Action</th>
+                            <th>Entity</th>
+                            <th>Entity ID</th>
+                            <th>User ID</th>
+                            <th>Username</th>
+                            <th>Role</th>
+                            <th>Details</th>
+                            <th>Timestamp</th>
+                        </tr>
+                    </thead>
+                    <tbody className="auditlogs_table-content">
+                        {auditLogs.map((item, index) => <DataRow key={index} auditLog={item} />)}
+                    </tbody>
+                </table>
             </div>
+        </div>
+    )
+}
+
+function DataRow({ auditLog }) {
+    return (
+        <tr>
+            <td>{auditLog.id}</td>
+            <td>{auditLog.action}</td>
+            <td>{auditLog.entity}</td>
+            <td>{auditLog.entityId}</td>
+            <td>{auditLog.userId}</td>
+            <td>{auditLog.username}</td>
+            <td>{auditLog.role}</td>
+            <td>{auditLog.details}</td>
+            <td>{auditLog.timestamp}</td>
+        </tr>
     )
 }
