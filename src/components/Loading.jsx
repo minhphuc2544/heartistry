ba49378a68afebb9d52660788867325e9d2d8aec
minhphuc2544 <phucle190904@gmail.com>
2024-12-25 09:58:11 +0700

    feat: loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,22 @@
+import "../styles/Loading.css";
+
+export default function Loading() {
+  return (
+    < div >
+    <div class="holder">
+      <div class="flip-preloader example-1">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div >
+  )
+    
+  
+ 
+}
+
+
